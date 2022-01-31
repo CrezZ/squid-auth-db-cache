a3632e79988aa6d9f438474af4f18846589aa47f
CrezZ <unpk@mail.ru>
2022-01-31 19:29:07 +0500

    change behavior

--- README.md
+++ README.md
@@ -1,21 +1,43 @@
+# squid-auth-db-cache
+
 Auth squid by query to oracle and cache result to mysql
 
 git clone 
 chown proxy -R squid-auth-db-cache
 
+#0 Install Oracle instant client
 
-#1 Install perl libs
+copy links from https://www.oracle.com/database/technologies/instant-client/linux-x86-64-downloads.html
+we need "Basic Package" and "SqlPlus"
 
-apt-get install build-essential default-libmysqlclient-dev`
+cd /opt
+mkdir oracle
+cd oracle
+
+wget https://download.oracle.com/otn_software/linux/instantclient/215000/instantclient-basic-linux.x64-21.5.0.0.0dbru.zip
+wget https://download.oracle.com/otn_software/linux/instantclient/215000/instantclient-sqlplus-linux.x64-21.5.0.0.0dbru.zip
+
+unzip instantclient*.zip
+rm *.zip
+
+echo /opt/oracle/instantclient_21_5 >/etc/ld.conf.d/oracle_client.conf
+/sbin/ldconfig
+echo 'export ORACLE_HOME=/opt/oracle/instantclient_21_5' >/etc/profile.d/oracle.sh
+
+
+#Install perl libs
+
+apt-get install build-essential default-libmysqlclient-dev
 export ORACLE_HOME=/opt/oracle/instantclient_21_5 
-cpan install DBI  Config::Simple CPAN::DistnameInfo LWP Test::NoWarnings DBD:Orable`
+cpan install DBI  Config::Simple CPAN::DistnameInfo LWP Test::NoWarnings DBD:Orable Log::Any
 
+# Init mysql
 
-mysql 
+mysql -e '
  create database squid_auth;
  CREATE USER 'squid'@'localhost' IDENTIFIED BY '123';
  GRANT ALL PRIVILEGES ON squid_auth.* TO 'squid'@'localhost';
  FLUSH PRIVILEGES;
+'
 
-
-# mysql squid_auth <init.sql
+ mysql squid_auth <init.sql
--- auth.pl
+++ auth.pl
@@ -4,75 +4,51 @@ use Config::Simple;
 use Digest::MD5 qw(md5 md5_hex md5_base64);
 use DBI;
 
+my $DEBUG=1;
+
 sub mysql_connect{
-### $dbh=mysql_connect (user, password, host, db)
-my $dsn = 'DBI:mysql:database='.$_[3].';host='.$_[2];
-#print $dsn;
-my $db_user_name = $_[0];
-my $db_password  = $_[1];
-my ($id, $password);
-#print 'pass='.$db_password.';';
-my $dbh = DBI->connect($dsn, $db_user_name, $db_password);
-return ($dbh);
+    my ($db_user_name, $db_password, $host, $db) = @_;
+    ### $dbh=mysql_connect (user, password, host, db)
+    my $dsn = 'DBI:mysql:database='.$db.';host='.$host;
+    my $dbh = DBI->connect($dsn, $db_user_name, $db_password);
+    return ($dbh);
 }
 
 sub oracle_connect{
 ### use $dbh=oracle_connect (user, password, host);
-my $dbh = DBI ->connect
-   ( 'dbi:Oracle:host=oracle.net.osu.ru;sid=OSU;port=1521', $_[0], $_[1],
+    my ($login,$pass,$host) = @_;
+  my $dbh = DBI ->connect
+   ( 'dbi:Oracle:host='.$host.';sid=OSU;port=1521', $login, $pass,
 	    {RaiseError => 1,
 	     AutoCommit => 0, PrintError=>1} ) or die 'ORACLE cannot connect';;
 return ($dbh);
 }
 
 sub oracle_query_one{
-## use $array=oracle_query_one ($dbh, $user);
-    my $oradb=$_[0];
-    my $sql = q{
+    my ($oradb,$func,$login,$realm)=@_;
+## use $array=oracle_query_one ($dbh, $func, $user, $realm);
+    my $sql = qq{
 	    BEGIN
-	        ASU_WEB.Get_LAN_Pwd_Hash(?,?,?,?);
+	        $func(?,?,?,?);
 	    END;
 };
     my $sth = $oradb->prepare( $sql );
-    $sth->bind_param(1,$_[2]);
-    $sth->bind_param_inout(2,\my $result,0);
-    $sth->bind_param_inout(3,\my $hash,0);
-    $sth->bind_param_inout(4,\my $mess,0);
+    $sth->bind_param(1,$login);
+    $sth->bind_param_inout(2,\my $result,1);
+    $sth->bind_param_inout(3,\my $hash,33);
+    $sth->bind_param_inout(4,\my $mess,100);
     $sth->execute();
     my @arr=($result,$hash, $mess);
      return @arr;
 }
 
 sub oracle_query_one_test{
+    my ($oradb,$func,$login,$realm)=@_;
     #test for any user with password '123'
-    my @arr=(1,md5_hex($_[2].':'.$_[3].':','123'));
+    my @arr=(1,md5_hex($login.':'.$realm.':'.'123'));
     return @arr;
 }
 
-sub mysql_query_one{
-    my ($mydb,$user,$realm,$ttl)= @_;
-    my $sth = $mydb->prepare(qq{select hash, lastdate from users where login=? and TIME_TO_SEC(TIMEDIFF(now(),lastdate))<?  });
-    $sth->bind_param(1,$user);
-    $sth->bind_param(1,$ttl);
-    $sth->execute();
-    my ($hash, $date) = $sth->fetchrow_array();
-    $sth->finish();
-    if ($hash){
-	return (1, $hash); 
-    } 
-    else
-    {
-	return (0); 
-    }
-}
-sub mysql_query_state{
-    my ($mydb)= @_;
-    my $sth = $mydb->prepare(qq{select state from state });
-    $sth->execute();
-    my ($state ) = $sth->fetchrow_array();
-    $sth->finish();
-	return $state; 
-}
 
 sub mysql_cache{
     my ($mydb,$user,$hash,$state)= @_;
@@ -90,20 +66,87 @@ sub mysql_cache{
 
 }
 
-sub sigtrap(){
+
+sub mysql_query_one{
+    my ($mydb,$user,$realm,$ttl,$ttl2,$DEBUG)= @_;
+        if ($DEBUG){open(F,"|/usr/bin/logger Search mysql..");close(F);}
+    my $sth = $mydb->prepare(qq{select hash, to_seconds(lastdate), users.state, state.state, to_seconds(now())
+			    from users 
+			    join state on (state.id=1)
+			    where login=?  });
+    $sth->bind_param(1,$user);
+    $sth->execute();
+    my ($hash, $date, $state, $globalstate, $now) = $sth->fetchrow_array();
+    $sth->finish();
+
+    if ($state != 0 && $globalstate==0) { #force deauth user
+        my $sth2 = $mydb->prepare("UPDATE users set state=0,lastdate=now() where login=? limit 1");
+        $sth2->bind_param(1,$user);
+        $sth2->execute();
+        $sth2->finish();
+        if ($DEBUG){open(F,"|/usr/bin/logger Made deauth");close(F);}
+	return (0,'');
+    }
+    if ($state == 0 && $globalstate==0 && $now-$date>$ttl2) { #restore deauth user for $ttl2 time
+        if ($DEBUG){open(F,"|/usr/bin/logger Expire TTL2");close(F);}
+        my $sth2 = $mydb->prepare("UPDATE users set state=1,lastdate=now() where login=? limit 1"); #TODO - use laststate
+        $sth2->bind_param(1,$user);
+        $sth2->execute();
+        $sth2->finish();
+        if ($DEBUG){open(F,"|/usr/bin/logger Made deauth");close(F);}
+	return (0,'');
+    }
+    if ($state == 0 && $globalstate==0 && $now-$date<$ttl2) { #restore deauth user for $ttl2 time
+        if ($DEBUG){open(F,"|/usr/bin/logger wait TTL2");close(F);}
+	return (1,$hash);
+    }
+    if ($now-$date > $ttl) { #// Expire cache
+	if ($DEBUG){open(F,"|/usr/bin/logger Expire TTL");close(F);}
+	return (20,'');
+    } 
+    if ($hash){
+        if ($DEBUG){open(F,"|/usr/bin/logger Valid hash");close(F);}
+	return ($state, $hash); 
+    }else
+    { 
+	if ($DEBUG){open(F,"|/usr/bin/logger Invalid hash");close(F);}
+        return (20,''); ##any more - go to oracle
+    }
+}
+
+
+sub mysql_query_state{
+    my ($mydb)= @_;
+    my $sth = $mydb->prepare(qq{select state from state });
+    $sth->execute();
+    my ($state ) = $sth->fetchrow_array();
+    $sth->finish();
+	return $state; 
+}
+
+
+sub sigtrap(){ # KILL handler
  print "Caught a signal\n";
  exit(1);
 }
 
+
+#########################################################################################################
+#########################################################################################################
+#########################################################################################################
+
+
 my $cfg = new Config::Simple('config.conf');
-my $oradb=oracle_connect($cfg->param('oracle_login'),$cfg->param('oracle_password'),$cfg->param('oracle_server'));
+
+my $oradb;
+#$oradb=oracle_connect($cfg->param('oracle_login'),$cfg->param('oracle_password'),$cfg->param('oracle_server'));
 my $mydb=mysql_connect($cfg->param('mysql_login'),
 			$cfg->param('mysql_password').'',
 			$cfg->param('mysql_server'),
 			$cfg->param('mysql_db'));
 
 $| = 1;
-use sigtrap 'handler' => \&sigtrap, 'HUP', 'INT','ABRT','QUIT','TERM';
+use sigtrap 'handler' => \&sigtrap, 'HUP', 'INT','ABRT','QUIT','TERM'; # KILL handler
 
 open(LOG,'>>log-debug.log');
 
@@ -112,11 +155,12 @@ my $time0= time();
 my $state=1; #permit all
 while (<>) {
 
+if ($DEBUG){open(F,"|/usr/bin/logger $_");close(F);}
+
 my $time = time();
 
 if ($time-$time0>1) # every 1 sec
-    {
-	$state=mysql_query_state($mydb);
+    {	$state=mysql_query_state($mydb);
     }
 
 my @input = split /[\"]/;
@@ -126,36 +170,45 @@ $input[3] =~ s/^\s+|\s+$//g; #realm
 
 #print LOG join(",", @input)."\n";
 
-my @hash;
-my @cache=mysql_query_one($mydb,$input[1],$input[3], $cfg->param('cache_time'));
-if ($cache[0]==1) {
+my @hash = (0,0);
+my @cache=mysql_query_one($mydb,$input[1],$input[3], $cfg->param('cache_time'),$cfg->param('deauth_time'),$DEBUG);
+
+if ($cache[0]==1) { # try search cache
+    if ($DEBUG){open(F,"|/usr/bin/logger Found valid cache");close(F);}
+
     @hash=(1,$cache[1]);
     #print 'cached';
 }
-else
-{
-    @hash=oracle_query_one_test($oradb, $cfg->param('oracle_func'),$input[1],$input[3]);
-    if ($hash[0]==1) {mysql_cache($mydb,$input[1],$hash[1],$hash[0]);}
-}
-#print LOG join(", ", @hash)."\n";
-print LOG gmtime().': LOGIN: '.$input[1].': RESULT: '.$hash[0];
+elsif ($cache[0]>10){ # if cache not found or expire try direct request
+    if ($DEBUG){open(F,"|/usr/bin/logger Request to ORACLE");close(F);}
 
-if ($hash[0]==1 && $state==1){ # ALL OK
+    @hash=oracle_query_one_test($oradb, $cfg->param('oracle_func'),$input[1],$input[3]);
+    if ($hash[0]==1) { # update cache
+	mysql_cache($mydb,$input[1],$hash[1],$hash[0]);
+    }
+} 
+
+#print LOG join(", ", @hash)."\n";
+print LOG gmtime().': LOGIN: '.$input[1].': RESULT: '.$hash[0]."\n";
+
+if ($hash[0]==1){ # ALL OK
     #print LOG $input[0].'OK ha1="'.$hash[1]."\"\n";
     print  $input[0].'OK ha1="'.$hash[1]."\"\n";
-    } else
-    {
-my $err='';
-if ($hash[0]==-3){$err='Invalid login';}
-if ($hash[0]==-2){$err='Password not set';}
-if ($hash[0]==-1){$err='Login must be lower case';}
-if ($hash[0]==0){$err='Invalid password';}
-if ($hash[0]==2){$err='Less 18 years';}
-
-print LOG $input[0].' ERR message="'.$err.'"'."\n";
-print $input[0].'ERR message="'.$err.'"'."\n";
-}
-
+    if ($DEBUG){open(F,"|/usr/bin/logger Return OK");close(F);}
+    } 
+else
+    {	#NOT ok
+    my $err='';
+    if ($hash[0]==-3){$err='Invalid login';}
+    if ($hash[0]==-2){$err='Password not set';}
+    if ($hash[0]==-1){$err='Login must be lower case';}
+    if ($hash[0]==0){$err='Invalid password';}
+    if ($hash[0]==2){$err='Less 18 years';}
+    
+    print LOG $input[0].' ERR message="'.$err.'"'."\n";
+    print $input[0].'ERR message="'.$err.'"'."\n";
+    if ($DEBUG){open(F,"|/usr/bin/logger Return Error $err");close(F);}
+    }
 }
 close(LOG);
 $oradb->disconnect();
